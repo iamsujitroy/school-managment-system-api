d35494fc1184df21da8bd84a92de8e7a998001f3
Sujit Roy <sujitroy78148@gmail.com>
2023-03-19 18:02:06 +0530

    api base `url` required

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -1,12 +1,27 @@
 const imageController = {};
 
 imageController.uploadSingleImage = async (req, res) => {
-    try {
-      const image = `image/${req.file.filename}`;
-      res.status(201).json({ success: true, message:"Image Uploaded successfully", imageUrl: image });
-    } catch (err) {
-      res.status(400).json({ message: err.message });
+  try {
+    if (process.env.API_BASE_URL) {
+      const image = `${process.env.API_BASE_URL}/image/${req.file.filename}`;
+      res
+        .status(201)
+        .json({
+          success: true,
+          message: "Image Uploaded successfully",
+          imageUrl: image,
+        });
+    } else {
+      res
+        .status(201)
+        .json({
+          success: false,
+          message: "server error: API base URL not found",
+        });
     }
-  };
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
 
 module.exports = imageController;
